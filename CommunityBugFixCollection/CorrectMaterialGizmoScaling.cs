0f7fb5c776278e49b19152cdc851da9aa1769c42
Arne Kiesewetter <banane9@live.de>
2024-12-19 00:35:43 +0100

    Add a fix for https://github.com/Yellow-Dog-Man/Resonite-Issues/issues/528

---
+++ CommunityBugFixCollection/CorrectMaterialGizmoScaling.cs
@@ -0,0 +1,23 @@
+﻿using Elements.Core;
+using FrooxEngine;
+using HarmonyLib;
+using MonkeyLoader.Resonite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommunityBugFixCollection
+{
+    [HarmonyPatchCategory(nameof(CorrectMaterialGizmoScaling))]
+    [HarmonyPatch(typeof(MaterialGizmo), nameof(MaterialGizmo.PositionInFrontOfUser))]
+    internal sealed class CorrectMaterialGizmoScaling : ResoniteMonkey<CorrectMaterialGizmoScaling>
+    {
+        public override bool CanBeDisabled => true;
+
+        // Apply default scale for inspector UI before it gets user-scaled again by PositionInFrontOfUser
+        private static void Prefix(MaterialGizmo __instance)
+            => __instance._inspectorRoot.Target.LocalScale = 0.0005f * float3.One;
+
+        private static bool Prepare() => Enabled;
+    }
+}
