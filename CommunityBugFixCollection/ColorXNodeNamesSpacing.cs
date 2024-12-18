6692d85f9eeccc54f1919192b3736a1491a4f6ca
Arne Kiesewetter <banane9@live.de>
2024-12-18 23:36:53 +0100

    Add a fix for the ColorX part of https://github.com/Yellow-Dog-Man/Resonite-Issues/issues/496

---
+++ CommunityBugFixCollection/ColorXNodeNamesSpacing.cs
@@ -0,0 +1,21 @@
+﻿using Elements.Core;
+using HarmonyLib;
+using MonkeyLoader.Resonite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommunityBugFixCollection
+{
+    [HarmonyPatchCategory(nameof(ColorXNodeNamesSpacing))]
+    [HarmonyPatch(typeof(StringHelper), nameof(StringHelper.BeautifyName))]
+    internal sealed class ColorXNodeNamesSpacing : ResoniteMonkey<ColorXNodeNamesSpacing>
+    {
+        public override bool CanBeDisabled => true;
+
+        private static string Postfix(string __result)
+            => __result.Replace("Color X", "ColorX ");
+
+        private static bool Prepare() => Enabled;
+    }
+}
