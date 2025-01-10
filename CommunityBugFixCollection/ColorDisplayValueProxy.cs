c74604201dab65a6607c2a434f411676b523b010
Arne Kiesewetter <banane9@live.de>
2025-01-10 23:31:09 +0100

    Add fix for https://github.com/Yellow-Dog-Man/Resonite-Issues/issues/557

---
+++ CommunityBugFixCollection/ColorDisplayValueProxy.cs
@@ -0,0 +1,33 @@
+﻿using Elements.Core;
+using FrooxEngine;
+using FrooxEngine.ProtoFlux;
+using FrooxEngine.ProtoFlux.Runtimes.Execution.Nodes;
+using HarmonyLib;
+using MonkeyLoader.Resonite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommunityBugFixCollection
+{
+    [HarmonyPatchCategory(nameof(ColorDisplayValueProxy))]
+    [HarmonyPatch(typeof(ValueDisplay<color>), nameof(ValueDisplay<color>.BuildContentUI))]
+    internal sealed class ColorDisplayValueProxy : ResoniteMonkey<ColorDisplayValueProxy>
+    {
+        public override bool CanBeDisabled => true;
+
+        private static void Postfix(ValueDisplay<color> __instance, ProtoFluxNodeVisual visual)
+        {
+            var colorToColorX = visual.Slot.GetComponentInChildren<ColorToColorX>();
+            var textFormatDriver = visual.Slot.GetComponentInChildren<MultiValueTextFormatDriver>();
+
+            if (colorToColorX is null || textFormatDriver is null)
+                return;
+
+            var valueProxySource = textFormatDriver.Slot.AttachComponent<ValueProxySource<color>>();
+            valueProxySource.Value.DriveFrom(__instance._value.Target);
+        }
+
+        private static bool Prepare() => Enabled;
+    }
+}
