08de6d88c1a42c3008d3f5ac186538b8b57d897c
Arne Kiesewetter <banane9@live.de>
2025-01-11 01:45:44 +0100

    Add a fix for https://github.com/Yellow-Dog-Man/Resonite-Issues/issues/3480

---
+++ CommunityBugFixCollection/PauseAnimatorUpdates.cs
@@ -0,0 +1,36 @@
+﻿using FrooxEngine;
+using HarmonyLib;
+using MonkeyLoader.Resonite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommunityBugFixCollection
+{
+    [HarmonyPatchCategory(nameof(PauseAnimatorUpdates))]
+    [HarmonyPatch(typeof(Animator), nameof(Animator.OnCommonUpdate))]
+    internal sealed class PauseAnimatorUpdates : ResoniteMonkey<PauseAnimatorUpdates>
+    {
+        public override bool CanBeDisabled => true;
+
+        private static bool Prefix(Animator __instance)
+        {
+            __instance._playback.ClipLength = (__instance.Clip.Asset?.Data?.GlobalDuration).GetValueOrDefault();
+
+            if (!__instance._fieldMappersValid)
+                __instance.GenerateFieldMappers();
+
+            if (__instance.IsPlaying)
+            {
+                var position = __instance.Position;
+
+                foreach (var fieldMapper in __instance._fieldMappers)
+                    fieldMapper.Set(position);
+            }
+
+            return false;
+        }
+
+        private static bool Prepare() => Enabled;
+    }
+}
