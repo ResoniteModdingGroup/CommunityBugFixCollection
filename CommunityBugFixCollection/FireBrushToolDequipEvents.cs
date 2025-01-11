044a4e3ffa64617ff4d7fe51b5d805d797cce56e
Arne Kiesewetter <banane9@live.de>
2025-01-11 18:46:14 +0100

    Add a fix for https://github.com/Yellow-Dog-Man/Resonite-Issues/issues/723

---
+++ CommunityBugFixCollection/FireBrushToolDequipEvents.cs
@@ -0,0 +1,29 @@
+﻿using FrooxEngine;
+using HarmonyLib;
+using MonkeyLoader.Resonite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CommunityBugFixCollection
+{
+    [HarmonyPatchCategory(nameof(FireBrushToolDequipEvents))]
+    [HarmonyPatch(typeof(BrushTool), nameof(BrushTool.OnDequipped))]
+    internal sealed class FireBrushToolDequipEvents : ResoniteMonkey<FireBrushToolDequipEvents>
+    {
+        public override bool CanBeDisabled => true;
+
+        private static bool Prepare() => Enabled;
+
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            return new[]
+            {
+                CodeInstruction.LoadArgument(0),
+                CodeInstruction.Call((Tool tool) => tool.OnDequipped())
+            }
+            .Concat(instructions);
+        }
+    }
+}
