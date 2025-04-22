8cc2d73e961b101e0820d3cea162ac87c753899a
Arne Kiesewetter <banane9@live.de>
2025-04-22 19:36:56 +0200

    Fix migrated items from messages not spawning because they still point to the old platform Adds a fix for https://github.com/Yellow-Dog-Man/Resonite-Issues/issues/83
    Co-authored-by: art0007i <art0007i@gmail.com>

---
+++ CommunityBugFixCollection/FixMigratedItemMessages.cs
@@ -0,0 +1,30 @@
+﻿using FrooxEngine;
+using HarmonyLib;
+using MonkeyLoader.Resonite;
+using SkyFrost.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+// Originally released under MIT-0 here:
+// https://github.com/art0007i/FixMigratedItemMessages
+
+namespace CommunityBugFixCollection
+{
+    [HarmonyPatchCategory(nameof(FixMigratedItemMessages))]
+    [HarmonyPatch(typeof(ContactsDialog), nameof(ContactsDialog.SpawnMessageItem))]
+    internal sealed class FixMigratedItemMessages : ResoniteMonkey<FixMigratedItemMessages>
+    {
+        private const string OldPrefix = "neosdb";
+
+        public override IEnumerable<string> Authors => Contributors.Art0007i;
+
+        public override bool CanBeDisabled => true;
+
+        public static void Prefix(ref Record record)
+        {
+            if (Enabled && record.AssetURI.StartsWith(OldPrefix))
+                record.AssetURI = $"{Engine.Current.PlatformProfile.DBScheme}{record.AssetURI[OldPrefix.Length..]}";
+        }
+    }
+}
