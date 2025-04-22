f5504bb97f14f4e0d471dc088c0d3a55947411ea
Arne Kiesewetter <banane9@live.de>
2025-04-22 18:26:49 +0200

    Prevents Grab World locomotion moving the player when activated Adds a fix for https://github.com/Yellow-Dog-Man/Resonite-Issues/issues/86
    Co-authored-by: art0007i <art0007i@gmail.com>

---
+++ CommunityBugFixCollection/StationaryGrabWorldActivation.cs
@@ -0,0 +1,35 @@
+﻿using FrooxEngine;
+using HarmonyLib;
+using MonkeyLoader.Resonite;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+// Originally released under MIT-0 here:
+// https://github.com/art0007i/FixGrabWorld
+
+namespace CommunityBugFixCollection
+{
+    [HarmonyPatchCategory(nameof(StationaryGrabWorldActivation))]
+    [HarmonyPatch(typeof(GrabWorldLocomotion), nameof(GrabWorldLocomotion.TryActivate))]
+    internal sealed class StationaryGrabWorldActivation : ResoniteMonkey<StationaryGrabWorldActivation>
+    {
+        public override IEnumerable<string> Authors => Contributors.Art0007i;
+
+        public override bool CanBeDisabled => true;
+
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> codes)
+        {
+            var directionReferenceGetter = AccessTools.PropertyGetter(typeof(ILocomotionReference), nameof(ILocomotionReference.DirectionReference));
+
+            foreach (var code in codes)
+            {
+                if (Enabled && code.Calls(directionReferenceGetter))
+                    code.operand = AccessTools.PropertyGetter(typeof(ILocomotionReference), nameof(ILocomotionReference.GripReference));
+
+                yield return code;
+            }
+        }
+    }
+}
