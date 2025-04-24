283758eadd5390d28ce634577aac388efb835dba
Arne Kiesewetter <banane9@live.de>
2025-04-24 21:57:17 +0200

    Use User cloud home template for groups Adds the proposed workaround / fix for https://github.com/Yellow-Dog-Man/Resonite-Issues/issues/1144
    Co-authored-by: goat <goatsdev@protonmail.com>

---
+++ CommunityBugFixCollection/GroupCloudHomeTemplate.cs
@@ -0,0 +1,29 @@
+﻿using FrooxEngine;
+using HarmonyLib;
+using MonkeyLoader.Resonite;
+using SkyFrost.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+// Initially done by https://github.com/goaaats
+
+namespace CommunityBugFixCollection
+{
+    [HarmonyPatchCategory(nameof(GroupCloudHomeTemplate))]
+    [HarmonyPatch(typeof(Userspace), nameof(Userspace.GetHomeTemplateUri))]
+    internal sealed class GroupCloudHomeTemplate : ResoniteMonkey<GroupCloudHomeTemplate>
+    {
+        public override IEnumerable<string> Authors => Contributors.Goat;
+
+        public override bool CanBeDisabled => true;
+
+        private static void Prefix(ref OwnerType ownerType)
+        {
+            // The template URL for group homes currently does not resolve.
+            // We change the owner type to User, so that we can create a group home with the standard cloud home template.
+            if (ownerType == OwnerType.Group)
+                ownerType = OwnerType.User;
+        }
+    }
+}
