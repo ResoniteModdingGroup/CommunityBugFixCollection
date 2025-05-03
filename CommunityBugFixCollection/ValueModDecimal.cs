e1772727d70c1e7411b131b4894d99a815e5535f
LazyTeenStudios <harleypeterson@gmail.com>
2025-05-03 12:33:45 -0500

    ValueMod Decimal fix added

---
+++ CommunityBugFixCollection/ValueModDecimal.cs
@@ -0,0 +1,28 @@
+﻿using Elements.Core;
+using FrooxEngine;
+using HarmonyLib;
+using MonkeyLoader.Resonite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommunityBugFixCollection
+{
+    [HarmonyPatchCategory(nameof(ValueModDecimal))]
+    [HarmonyPatch(typeof(Elements.Core.Coder<Decimal>), nameof(Elements.Core.Coder<Decimal>.Mod))]
+    internal sealed class ValueModDecimal : ResoniteMonkey<ValueModDecimal>
+    {
+        public override IEnumerable<string> Authors => Contributors.__Choco__;
+
+        public override bool CanBeDisabled => true;
+
+        private static bool VarDecMod(Decimal a, Decimal b)
+        {
+            if (b == 0)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
