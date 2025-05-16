80b555f10d542477cb217b1da08f5d503ea790dd
Arne Kiesewetter <banane9@live.de>
2025-05-16 23:11:32 +0200

    Make NaN floats / doubles never equal to each other for the ProtoFlux == and != nodes, as well as the ValueEqualityDriver component Adds a fix for https://github.com/Yellow-Dog-Man/Resonite-Issues/issues/1046

---
+++ CommunityBugFixCollection/NaNtEqual.cs
@@ -0,0 +1,114 @@
+﻿using Elements.Core;
+using FrooxEngine;
+using HarmonyLib;
+using MonkeyLoader.Resonite;
+using ProtoFlux.Core;
+using ProtoFlux.Runtimes.Execution;
+using ProtoFlux.Runtimes.Execution.Nodes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommunityBugFixCollection
+{
+    [HarmonyPatch]
+    [HarmonyPatchCategory(nameof(NaNtEqual))]
+    internal sealed class NaNtEqual : ResoniteMonkey<NaNtEqual>
+    {
+        public override IEnumerable<string> Authors => Contributors.Banane9;
+
+        public override bool CanBeDisabled => true;
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(ValueEqualityDriver<double>), nameof(ValueEqualityDriver<double>.OnChanges))]
+        private static bool ValueEqualityDriverDoublePrefix(ValueEqualityDriver<double> __instance)
+        {
+            if (!Enabled)
+                return true;
+
+            if (!__instance.Target.IsLinkValid)
+                return false;
+
+            var value = __instance.TargetValue.Target?.Value ?? default;
+
+            var areEqual = (!__instance.UseApproximateComparison.Value || !Coder<double>.SupportsApproximateComparison)
+                ? value == __instance.Reference.Value
+                : Coder<double>.Approximately(value, __instance.Reference.Value);
+
+            // XOR inverts the other bool
+            __instance.Target.Target.Value = __instance.Invert.Value ^ areEqual;
+
+            return false;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(ValueEqualityDriver<float>), nameof(ValueEqualityDriver<float>.OnChanges))]
+        private static bool ValueEqualityDriverFloatPrefix(ValueEqualityDriver<float> __instance)
+        {
+            if (!Enabled)
+                return true;
+
+            if (!__instance.Target.IsLinkValid)
+                return false;
+
+            var value = __instance.TargetValue.Target?.Value ?? default;
+
+            var areEqual = (!__instance.UseApproximateComparison.Value || !Coder<float>.SupportsApproximateComparison)
+                ? value == __instance.Reference.Value
+                : Coder<float>.Approximately(value, __instance.Reference.Value);
+
+            // XOR inverts the other bool
+            __instance.Target.Target.Value = __instance.Invert.Value ^ areEqual;
+
+            return false;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(ValueEquals<double>), nameof(ValueEquals<double>.Compute))]
+        private static bool ValueEqualsDoublePrefix(ExecutionContext context, ref bool __result)
+        {
+            if (!Enabled)
+                return true;
+
+            __result = 0.ReadValue<double>(context) == 1.ReadValue<double>(context);
+
+            return false;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(ValueEquals<float>), nameof(ValueEquals<float>.Compute))]
+        private static bool ValueEqualsFloatPrefix(ExecutionContext context, ref bool __result)
+        {
+            if (!Enabled)
+                return true;
+
+            __result = 0.ReadValue<float>(context) == 1.ReadValue<float>(context);
+
+            return false;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(ValueNotEquals<double>), nameof(ValueNotEquals<double>.Compute))]
+        private static bool ValueNotEqualsDoublePrefix(ExecutionContext context, ref bool __result)
+        {
+            if (!Enabled)
+                return true;
+
+            __result = 0.ReadValue<double>(context) != 1.ReadValue<double>(context);
+
+            return false;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(ValueNotEquals<float>), nameof(ValueNotEquals<float>.Compute))]
+        private static bool ValueNotEqualsFloatPrefix(ExecutionContext context, ref bool __result)
+        {
+            if (!Enabled)
+                return true;
+
+            __result = 0.ReadValue<float>(context) != 1.ReadValue<float>(context);
+
+            return false;
+        }
+    }
+}
