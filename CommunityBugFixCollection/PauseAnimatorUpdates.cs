f4b4706d3bf67e44278f8a7bcdbf2343e68079ec
989onan <onanchew@gmail.com>
2025-07-02 17:31:23 -0400

    Animator no longer broke
    I took personal offense that my mantis blade that was driven didn't work with
    this mod, so I fixed it.

--- CommunityBugFixCollection/PauseAnimatorUpdates.cs
+++ CommunityBugFixCollection/PauseAnimatorUpdates.cs
@@ -3,6 +3,7 @@ using HarmonyLib;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Runtime.CompilerServices;
 
 namespace CommunityBugFixCollection
 {
@@ -11,6 +12,24 @@ namespace CommunityBugFixCollection
     internal sealed class PauseAnimatorUpdates : ResoniteBugFixMonkey<PauseAnimatorUpdates>
     {
         public override IEnumerable<string> Authors => Contributors.Banane9;
+
+        private class Float
+        {
+            public float Value;
+
+            public Float(float value)
+            {
+                Value = value;
+            }
+
+            public Float()
+            {
+                Value = 0f;
+            }
+        }
+
+
+        private static readonly ConditionalWeakTable<Animator, Float> _hasChangedPlayhead = new();
 
         private static bool Prefix(Animator __instance)
         {
@@ -22,9 +41,11 @@ namespace CommunityBugFixCollection
             if (!__instance._fieldMappersValid)
                 __instance.GenerateFieldMappers();
 
-            if (__instance.IsPlaying)
+
+            if (_hasChangedPlayhead.GetOrCreateValue(__instance).Value != __instance._playback.Position)
             {
                 var position = __instance.Position;
+                _hasChangedPlayhead.GetOrCreateValue(__instance).Value = __instance.Position;
 
                 foreach (var fieldMapper in __instance._fieldMappers)
                     fieldMapper.Set(position);
@@ -33,4 +54,6 @@ namespace CommunityBugFixCollection
             return false;
         }
     }
+
+    
 }
