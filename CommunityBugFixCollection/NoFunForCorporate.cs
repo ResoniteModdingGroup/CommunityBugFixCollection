98514119107172f2efe1da0ac91be963972c8360
Arne Kiesewetter <banane9@live.de>
2026-03-05 22:14:45 +0100

    Prevent April Fools content from triggering for users in universes with optional setting to force april fools at any time.
    This adds a fix for https://github.com/Yellow-Dog-Man/Resonite-Issues/issues/4016 - If you're reading this, Prime, I hope this motivates you to fix it in vanilla before the end of the month, too ;)

---
+++ CommunityBugFixCollection/NoFunForCorporate.cs
@@ -0,0 +1,39 @@
+﻿using FrooxEngine;
+using HarmonyLib;
+
+namespace CommunityBugFixCollection
+{
+    [HarmonyPatch]
+    [HarmonyPatchCategory(nameof(NoFunForCorporate))]
+    internal sealed class NoFunForCorporate : ResoniteBugFixMonkey<NoFunForCorporate>
+    {
+        public override IEnumerable<string> Authors => Contributors.Banane9;
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(Engine), nameof(Engine.IsAprilFools), MethodType.Getter)]
+        private static bool EngineIsAprilFoolsPrefix(ref bool __result)
+        {
+            if (!Enabled)
+                return true;
+
+            if (ConfigSection.ForceAprilFools)
+            {
+                __result = true;
+                return false;
+            }
+
+            if (Engine.Current.InUniverse)
+            {
+                __result = false;
+                return false;
+            }
+
+            return true;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(ViolentAprilFoolsExplosion), nameof(ViolentAprilFoolsExplosion.OnDestroying))]
+        private static bool ViolentAprilFoolsExplosionOnDestroyingPrefix()
+            => !Enabled || ConfigSection.ForceAprilFools || !Engine.Current.InUniverse;
+    }
+}
