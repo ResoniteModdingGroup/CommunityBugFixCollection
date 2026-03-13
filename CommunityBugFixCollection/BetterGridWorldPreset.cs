48b91e34eba952f40b52747b3792eabbc01eaa27
Arne Kiesewetter <banane9@live.de>
2026-03-13 10:57:22 +0100

    Update GridWorld Preset for new resonite version and make collider thicker
    Fixes #71; Fixes #81

--- CommunityBugFixCollection/BetterGridWorldPreset.cs
+++ CommunityBugFixCollection/BetterGridWorldPreset.cs
@@ -1,5 +1,8 @@
 ﻿using Elements.Core;
 using FrooxEngine;
+using FrooxEngine.FrooxEngine.ProtoFlux.CoreNodes;
+using FrooxEngine.ProtoFlux.Runtimes.Execution.Nodes;
+using FrooxEngine.ProtoFlux.Runtimes.Execution.Nodes.Operators;
 using HarmonyLib;
 using Renderite.Shared;
 
@@ -16,14 +19,16 @@ namespace CommunityBugFixCollection
             if (!Enabled)
                 return true;
 
-            WorldPresets.BlankWorld(w);
+            WorldPresets.SetupWorldControllers(w);
+            WorldPresets.SetupSimpleSkyBox(w, WorldPresets.SetupSimpleWorldLighting(w));
 
             var ground = w.AddSlot("Ground");
-            ground.LocalRotation = floatQ.Euler(90, 0, 0);
 
             var attachedModel = ground.AttachMesh<GridMesh, PBS_Metallic>();
-            attachedModel.mesh.Size.Value = 1000 * float2.One;
+            attachedModel.mesh.Facing = float3.Up;
             attachedModel.mesh.Points.Value = 20 * int2.One;
+            attachedModel.mesh.Size.Value = 1000 * float2.One;
+            attachedModel.mesh.UVScale.DriveFrom(attachedModel.mesh.Size, true);
 
             var gridTexture = ground.AttachComponent<GridTexture>();
             gridTexture.BackgroundColor.Value = new colorX(0.07f, 0.08f, 0.11f);
@@ -45,14 +50,31 @@ namespace CommunityBugFixCollection
             majorGrid.Offset.Value = int2.Zero;
 
             attachedModel.material.AlbedoTexture.Target = gridTexture;
-            attachedModel.material.TextureScale.DriveFrom(attachedModel.mesh.Size);
             attachedModel.material.Smoothness.Value = 0f;
 
             var boxCollider = ground.AttachComponent<BoxCollider>();
-            var swizzle = ground.AttachComponent<Float2ToFloat3SwizzleDriver>();
-            swizzle.Source.Target = attachedModel.mesh.Size;
-            swizzle.Target.Target = boxCollider.Size;
+            boxCollider.Offset.Value = new(0, -0.5f, 0);
             boxCollider.SetCharacterCollider();
+
+            var swizzleSlot = ground.AddSlot("Collider Swizzle");
+
+            var meshSizeSource = swizzleSlot.AttachComponent<ValueSource<float2>>();
+            meshSizeSource.TrySetRootSource(attachedModel.mesh.Size);
+
+            var unpackSize = swizzleSlot.AttachComponent<Unpack_Float2>();
+            unpackSize.V.Target = meshSizeSource;
+
+            var oneConstant = swizzleSlot.AttachComponent<ValueInput<float>>();
+            oneConstant.Value.Value = 1;
+
+            var packSize = swizzleSlot.AttachComponent<Pack_Float3>();
+            packSize.X.Target = unpackSize.X;
+            packSize.Y.Target = oneConstant;
+            packSize.Z.Target = unpackSize.Y;
+
+            var colliderSizeDrive = swizzleSlot.AttachComponent<ValueFieldDrive<float3>>();
+            colliderSizeDrive.TrySetRootTarget(boxCollider.Size);
+            colliderSizeDrive.Value.Target = packSize;
 
             return false;
         }
