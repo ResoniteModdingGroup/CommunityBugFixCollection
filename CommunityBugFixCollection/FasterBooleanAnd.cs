823d6a052a5cf0fae5faf545321c28a751d3657e
Arne Kiesewetter <banane9@live.de>
2026-06-13 21:12:39 +0200

    Test for different boolean AND evaluation - doesn't work though

---
+++ CommunityBugFixCollection/FasterBooleanAnd.cs
@@ -0,0 +1,28 @@
+﻿using ProtoFlux.Runtimes.Execution.Nodes.Operators;
+using HarmonyLib;
+
+using ExecutionContext = ProtoFlux.Runtimes.Execution.ExecutionContext;
+
+namespace CommunityBugFixCollection
+{
+    [HarmonyPatchCategory(nameof(FasterBooleanAnd))]
+    [HarmonyPatch(typeof(AND_Bool), nameof(AND_Bool.Compute))]
+    internal sealed class FasterBooleanAnd : ResoniteBugFixMonkey<FasterBooleanAnd>
+    {
+        public override IEnumerable<string> Authors => Contributors.Banane9;
+
+        private static bool Prefix(AND_Bool __instance, ExecutionContext context, out bool __result)
+        {
+            __result = false;
+            var aSource = __instance.A.Source;
+
+            if (aSource is null || context.CurrentRuntime.EvaluateValue<bool>(aSource, context))
+            {
+                var bSource = __instance.B.Source;
+                __result = bSource is null || context.CurrentRuntime.EvaluateValue<bool>(bSource, context);
+            }
+
+            return false;
+        }
+    }
+}
