a7851a4e941105cafc8e6832bb26ac9169f31c21
Arne Kiesewetter <banane9@live.de>
2026-09-11 19:24:18 +0200

    Add Re-Upgrade Dash Monkey and add German names for Monkeys

---
+++ CommunityBugFixCollection/ReUpgradeDash.cs
@@ -0,0 +1,26 @@
+﻿using FrooxEngine;
+
+namespace CommunityBugFixCollection
+{
+    internal sealed class ReUpgradeDash : ResoniteBugFixMonkey<ReUpgradeDash>
+    {
+        public override IEnumerable<string> Authors => Contributors.Banane9;
+
+        protected override bool OnComputeDefaultEnabledState()
+            => false;
+
+        protected override void OnEnabled()
+        {
+            base.OnEnabled();
+
+            var userspaceDash = Userspace.Current.World
+                .GetGloballyRegisteredComponent<UserspaceRadiantDash>();
+
+            userspaceDash.Slot.ForeachComponentInChildren<RadiantDashScreen>(
+                dashScreen => dashScreen.CleanupOldComponents(RadiantDashScreen.DASH_SCREENS_CLEANUP_VERSION),
+                cacheItems: true);
+
+            Enabled = false;
+        }
+    }
+}
